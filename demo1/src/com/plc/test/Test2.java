bb830ec53fe6069ccbfe77e27f0c3a6f8977c8f6
PLCodeGo <1214784718@qq.com>
2023-03-04 15:26:13 +0800

    hello，第二次，加了一句“修改文件”，但好像没提交成功

--- demo1/src/com/plc/test/Test2.java
+++ demo1/src/com/plc/test/Test2.java
@@ -4,5 +4,6 @@ public class Test2 {
     public static void main(String[] args) {
         System.out.println("hahahah");
         System.out.println("修改文件");
+        System.out.println("没提交成功吗？");
     }
 }
